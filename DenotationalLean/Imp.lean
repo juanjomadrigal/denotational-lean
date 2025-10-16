3d874981e1449a035adeb285b4f13ec7f528c981
juanjomadrigal <juanjomadrigal326@gmail.com>
2025-10-16 09:12:50 +0200

    imp language

---
+++ DenotationalLean/Imp.lean
@@ -0,0 +1,24 @@
+
+abbrev Loc := String
+
+inductive Aexp : Type where
+  | Nat : Nat -> Aexp
+  | Loc : Loc -> Aexp
+  | Add : Aexp -> Aexp -> Aexp
+  | Sub : Aexp -> Aexp -> Aexp
+  | Mul : Aexp -> Aexp -> Aexp
+
+inductive Bexp : Type where
+  | Bool : Bool -> Bexp
+  | Eq : Aexp -> Aexp -> Bexp
+  | Ge : Aexp -> Aexp -> Bexp
+  | Not : Bexp -> Bexp
+  | And : Bexp -> Bexp -> Bexp
+  | Or  : Bexp -> Bexp -> Bexp
+
+inductive Com : Type where
+  | Skip : Com
+  | Assing : Loc -> Aexp -> Com
+  | Then : Com -> Com -> Com
+  | Ite : Bexp -> Com -> Com -> Com
+  | While : Bexp -> Com -> Com
